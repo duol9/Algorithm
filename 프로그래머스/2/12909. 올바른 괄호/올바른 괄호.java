1dca577de6a90c3df939a285f17793098664696f
HAYEONG <90500100+duol9@users.noreply.github.com>
2025-01-13 20:18:38 +0900

    [level 2] Title: 올바른 괄호, Time: 18.95 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+    
+        for (char ch : s.toCharArray()) { 
+            if (ch == '(') {
+                stack.push(ch); 
+            } else { 
+                if (stack.isEmpty()) {
+                    return false; 
+                }
+                stack.pop(); 
+            }
+        }
+        return stack.isEmpty();
+    }
+}
